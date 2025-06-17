c25c37d3ff0d6efca13d1b5511e052266037d3a7
senn59 <74777070+senn59@users.noreply.github.com>
2025-06-17 20:54:36 +0200

    feat: add PI controller

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,25 @@ Verify your installation together with the Bala2 robot by programming the runnin
 #include <M5Stack.h>
 #include <Wire.h>
 
+#define MAX_VAL 1023
+// #define MAX_INTEGRAL 1000
+
+float target_velocity_left = 0;
+float target_velocity_right = 0;
+
+int prev_pos_left = 0;
+int prev_pos_right = 0;
+
+float sample_time = 0.05;
+float Kp = 0.05;
+float Ki = 0.1;
+float integral_err_left = 0;
+float integral_err_right = 0;
+float dt = 0.05;
+
+
+
+
 void read_encoders(int32_t* wheel_left_encoder, int32_t* wheel_right_encoder)
 {
   uint8_t data_in[8];
@@ -46,6 +65,52 @@ void write_motor_cmd(int16_t wheel_left, int16_t wheel_right) // Sets the motor 
   data_out[3] = (int8_t)(wheel_right >> 0);
 
   M5.I2C.writeBytes(0x3A, 0x00, data_out, 4);
+}
+
+void read_motor_velocity(int32_t* left, int32_t* right)
+{
+  int curr_pos_left, curr_pos_right;
+  read_encoders(&curr_pos_left, &curr_pos_right);
+
+  *left = (curr_pos_left - prev_pos_left) / dt;
+  *right = (curr_pos_right - prev_pos_right) / dt;
+
+  prev_pos_left = curr_pos_left;
+  prev_pos_right = curr_pos_right;
+}
+
+void pi_control()
+{
+  int32_t velocity_left, velocity_right;
+  read_motor_velocity(&velocity_left, &velocity_right);
+
+  //error calc
+  float err_left = target_velocity_left - velocity_left;
+  float err_right = target_velocity_right - velocity_right;
+
+  integral_err_left += err_left * dt;
+  integral_err_right += err_right * dt;
+
+  // integral_err_left = constrain(integral_err_left, -1000, 1000);
+  // integral_err_right = constrain(integral_err_right, -1000, 1000);
+
+  //output
+  float output_left = err_left * Kp + integral_err_left * Ki;
+  float output_right = err_right * Kp + integral_err_right * Ki;
+
+  int pwm_left = constrain((int)output_left, -1023, 1023);
+  int pwm_right = constrain((int)output_right, -1023, 1023);
+  write_motor_cmd(pwm_left, pwm_right);
+  Serial.println("--------------");
+  Serial.print("Desired velocity (ticks/s): ");
+  Serial.println(target_velocity_left);
+  Serial.print("Measured velocity (ticks/s): ");
+  Serial.println(velocity_left);
+  Serial.print("Control output (PWM): ");
+  Serial.println(pwm_left);
+  Serial.print("Integral err: ");
+  Serial.println(integral_err_left);
+  Serial.println("--------------");
 }
 
 void setup() {
@@ -86,13 +151,21 @@ void setup() {
 }
 
 void loop() {
-  int32_t left, right;
- 
-  read_encoders(&left, &right);
-  Serial.print(left);
-  Serial.print(", ");
-  Serial.println(right);
+  static uint32_t start_time = millis();
+  static uint32_t last_sample_time = start_time;
+  uint32_t now = millis();
+  if (now - last_sample_time >= sample_time * 1000) {
+    last_sample_time += sample_time * 1000;
+    int32_t elapsed = now - start_time;
 
-  write_motor_cmd(0, left);
-  delay(1000);
+    if (elapsed > 5000)
+    {
+      target_velocity_left = 0;
+      target_velocity_right = 0;
+    } else {
+      target_velocity_left = 1880;
+      target_velocity_right = 1880;
+    }
+    pi_control();
+  }
 }
