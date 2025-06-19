bbfa463e933d58d48dd14c74784503f706dbee29
timdbie <timdbie@gmail.com>
2025-06-19 22:52:49 +0200

    feat: Implemented infrared logic

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,9 @@ Verify your installation together with the Bala2 robot by programming the runnin
 #define MAX_VAL 1023
 // #define MAX_INTEGRAL 1000
 
+#define LEFT_IR 26
+#define RIGHT_IR 36
+
 float target_velocity_left = 0;
 float target_velocity_right = 0;
 float Kp = 0.05;
@@ -44,6 +47,9 @@ void setup() {
 
   Serial.begin(115200);
   Serial.println("Hello from M5Stack (serial port)!");
+
+  pinMode(LEFT_IR, INPUT_PULLUP);
+  pinMode(RIGHT_IR, INPUT_PULLUP);
   
   if(i2cServoInit())
   {
@@ -60,6 +66,35 @@ void setup() {
 void loop() {
   uint32_t elapsed;
   if (intervalReady(&elapsed)) {
+    int left_sensor = digitalRead(LEFT_IR);
+    int right_sensor = digitalRead(RIGHT_IR);
+
+    if (left_sensor == HIGH && right_sensor == LOW) {
+      // line detected on left sensor only
+      target_velocity_left = 0;  
+      target_velocity_right = 200; 
+    }
+    else if (right_sensor == HIGH && left_sensor == LOW) {
+      // line detected on right sensor only
+      target_velocity_left = 200; 
+      target_velocity_right = 0;  
+    }
+    else if (left_sensor == LOW && right_sensor == LOW) {
+      // no line detected
+      target_velocity_left = 0;
+      target_velocity_right = 0;
+    }
+    else if (left_sensor == HIGH && right_sensor == HIGH) {
+      // line detected
+      target_velocity_left = 200;
+      target_velocity_right = 200;
+    }
+    else {
+      // fallback in case of unexpected sensor readings
+      target_velocity_left = 0;
+      target_velocity_right = 0;
+    }
+
     int32_t velocity_left;
     int32_t velocity_right;
     pi_control(target_velocity_left, target_velocity_right, &velocity_left, &velocity_right);
